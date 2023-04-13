bff024684c762d060a1da0664a94758f58ae8ad6
cuiruijunphp <cuiruijunphp@163.com>
2023-04-13 09:19:46 +0800

    去除两端指定字符方法

---
+++ src/main/java/com/cui/demo/util/StringDealUtil.java
@@ -0,0 +1,33 @@
+package com.cui.demo.util;
+
+public class StringDealUtil {
+
+    /**
+     * 去除首位的占位符
+     * @param str      要处理的字符串
+     * @param trimStr  要去除的字符串
+     * @param pos      left-左,right-右,all-左右都删除
+     * @return
+     */
+    public static String trimStr(String str, String trimStr, String pos){
+        int length = trimStr.length();
+        int leftSubPos = 0;
+        int rightSubPos = str.length();
+        if(pos.equals("left") || pos.equals("all")){
+            //处理左边的字符
+             int leftPos = str.indexOf(trimStr);
+             if(leftPos == 0){
+                 leftSubPos = length;
+             }
+        }
+
+        if(pos.equals("right") || pos.equals("all")){
+            //处理右边的字符
+            int rightPos = str.lastIndexOf(trimStr);
+            if(rightPos == (str.length() - length)){
+                rightSubPos = rightPos;
+            }
+        }
+        return str.substring(leftSubPos, rightSubPos);
+    }
+}
